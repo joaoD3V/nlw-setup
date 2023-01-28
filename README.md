8885aacd6162c285655b30ba286a6a5656655ae1
Joao Silva <joao.devweb@gmail.com>
2023-01-28 09:53:22 -0300

    Finish NLW :rocket:

---
+++ README.md
@@ -0,0 +1,56 @@
+<p align="center">
+  <img alt="" src=".github/capa.png" width="100%">
+</p>
+
+<p align="center">
+  <img alt="" src=".github/ERD.png" width="100%">
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+# NLW - Setup
+
+## 🚀 Tecnologias
+
+### Front-end
+- HTML
+- CSS
+- TypeScript
+- ReactJS
+- TailwindCSS
+- Radix UI
+- DayJS
+
+### Back-end
+- NodeJS
+- Fastify
+- TypeScript
+- Prisma
+- SQLite
+- Zod
+- DayJS
+
+### Mobile
+- React Native
+- Expo
+- Native Wind
+- React Navigation
+- React Native Reanimated
+- DayJS
+
+
+## 🎨 Layout:
+
+Figma: https://www.figma.com/file/t87XssDxaY15o7YMe8s9ri/Habits-(i)-(Community)?node-id=6%3A910&t=BML5NmFvTQecd5i3-1
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para mais detalhes.
+
+---
+
+Feito com ♥ by joaoD3V
---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,72 @@
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
+import { api } from '../lib/axios';
+import { generateDatesFromYearBeginning } from '../utils/generateDatesFromYearBeginning';
+import { HabitDay } from './HabitDay';
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+const summaryDates = generateDatesFromYearBeginning();
+
+const minimumSummaryDatesSize = 18 * 7; // 18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
+
+type Summary = {
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+};
+
+//Array.from cria um novo array com a quantidade que desejar
+
+export function SummaryTable() {
+  const [summary, setSummary] = useState<Summary[]>([]);
+
+  useEffect(() => {
+    api.get<Summary[]>('/summary').then((response) => {
+      setSummary(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, index) => (
+          <span
+            key={`${weekDay}-${index}`}
+            className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+          >
+            {weekDay}
+          </span>
+        ))}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-2">
+        {summary.length > 0 &&
+          summaryDates.map((date) => {
+            const dayInSummary = summary.find((day) =>
+              dayjs(date).isSame(day.date, 'day')
+            );
+
+            return (
+              <HabitDay
+                key={date.toString()}
+                date={date}
+                amount={dayInSummary?.amount}
+                defaultCompleted={dayInSummary?.completed}
+              />
+            );
+          })}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+            <div
+              key={index}
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
